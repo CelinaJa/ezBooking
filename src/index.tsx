95193ab6bc8ab56b395ea5447c3e26ad7b368f85
Celina Jakobsen <celina@quick.no>
2020-08-31 18:06:04 +0200

    Add Firebase

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyDPk-m7lOvjurlMnqh8E7b5gBgaZWNv9h8',
+  authDomain: 'ezbooking-c.firebaseapp.com',
+  databaseURL: 'https://ezbooking-c.firebaseio.com',
+  projectId: 'ezbooking-c',
+  storageBucket: 'ezbooking-c.appspot.com',
+  messagingSenderId: '646603043364',
+  appId: '1:646603043364:web:354d0ca3211da05b3ddeb1',
+  measurementId: 'G-6Q0Z4VQ5DF',
+};
+
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
